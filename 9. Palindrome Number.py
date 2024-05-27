f81ce73ea1186916c3decf57d38bf2a1e436f4c5
Vikash Kumar <vkumar0961@gmail.com>
2024-05-28 00:35:43 +0530

    2nd method

--- 9. Palindrome Number.py
+++ 9. Palindrome Number.py
@@ -19,4 +19,19 @@ class Solution:
             return False
         
 
+# Method 2
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x<0:
+            return False
+        
+        s=str(x)
+        for i in range(len(s)//2):
+            if(s[i]!=s[len(s)-i-1]):
+                return False
+            
+        return True
+        
+
+        
         
